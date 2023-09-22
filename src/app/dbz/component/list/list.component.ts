63b0ddc08e2f4e746dd1a9ded228afe2bea82bac
Manuel <Tu correo>
2023-09-22 09:01:38 -0600

    Fresh Start

---
+++ src/app/dbz/component/list/list.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[]= [{
+    name:'Trunks',
+    power:10,
+  }]
+
+  onDeleteCharacter(index:number):void{
+
+    console.log({index})
+  }
+
+}
